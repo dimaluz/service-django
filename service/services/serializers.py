d5337ad7c3c5682128f959a3fbe12acb89549353
dimaluz <dimaluz@rambler.ru>
2024-06-06 15:58:05 +0100

    Celery was set up

--- service/services/serializers.py
+++ service/services/serializers.py
@@ -2,10 +2,21 @@ from rest_framework import serializers
 from . import models
 
 
+class PlanSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Plan
+        fields = ('__all__')
+
+
 class SubscriptionSerializer(serializers.ModelSerializer):
+    plan = PlanSerializer()
     client_name = serializers.CharField(source='client.company_name')
     email = serializers.CharField(source='client.user.email')
+    price = serializers.SerializerMethodField()
+
+    def get_price(self, subscription):
+        return subscription.price
 
     class Meta:
         model = models.Subscription
-        fields = ('id', 'plan_id', 'client_name', 'email')
+        fields = ('id', 'plan_id', 'client_name', 'email', 'plan', 'price')
