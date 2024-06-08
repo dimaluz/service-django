a4fa087b269a8fe3d5f013ec1aba9f93c2dde8dc
dimaluz <dimaluz@rambler.ru>
2024-06-08 14:25:16 +0100

    Second worker was added

--- service/services/models.py
+++ service/services/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.core.validators import MaxValueValidator
 
 from clients.models import Client
-from .tasks import set_price
+from .tasks import set_price, set_comment
 
 class Service(models.Model):
     name = models.CharField(max_length=50)
@@ -16,6 +16,7 @@ class Service(models.Model):
         if self.full_price != self.__full_price:
             for subscription in self.subscriptions.all():
                 set_price.delay(subscription.id)
+                set_comment.delay(subscription.id)
         return super().save(*args, **kwargs)
 
     def __str__(self):
@@ -41,6 +42,7 @@ class Plan(models.Model):
         if self.discount_percent != self.__discount_percent:
             for subscription in self.subscriptions.all():
                 set_price.delay(subscription.id)
+                set_comment.delay(subscription.id)
         return super().save(*args, **kwargs)
 
     def __str__(self):
@@ -53,5 +55,6 @@ class Subscription(models.Model):
     plan = models.ForeignKey(Plan, related_name='subscriptions', on_delete=models.PROTECT)
 
     price = models.PositiveIntegerField(default=0)
+    comment = models.CharField(max_length=50, default='')
 
     
--- service/services/tasks.py
+++ service/services/tasks.py
@@ -1,6 +1,8 @@
 from celery import shared_task
 from django.db.models import F
 from celery_singleton import Singleton
+from django.db import transaction
+import time, datetime
 
 
 
@@ -9,6 +11,23 @@ def set_price(subscription_id):
     
     from services.models import Subscription
 
-    subscription = Subscription.objects.filter(id=subscription_id).annotate(annotated_price=F('service__full_price') - F('service__full_price') * F('plan__discount_percent') / 100.00).first()
-    subscription.price = subscription.annotated_price
-    subscription.save()
+    with transaction.atomic():
+
+        subscription = Subscription.objects.select_for_update().filter(id=subscription_id).annotate(annotated_price=F('service__full_price') - F('service__full_price') * F('plan__discount_percent') / 100.00).first()
+
+        subscription.price = subscription.annotated_price
+        subscription.save()
+
+
+@shared_task(base=Singleton)
+def set_comment(subscription_id):
+    
+    from services.models import Subscription
+   
+
+    with transaction.atomic():
+        subscription = Subscription.objects.select_for_update().get(id=subscription_id)
+
+        subscription.comment = str(datetime.datetime.now())
+        subscription.save()
+    
